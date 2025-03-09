a2a9b48904fdf046501bbf38829915c2fa42d602
PushpakAg <pushpakagrawal123@gmail.com>
2025-03-10 04:52:26 +0530

    fixed cors

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 import instaloader
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 # Initialize Instaloader
 L = instaloader.Instaloader()
 
