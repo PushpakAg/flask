fddea7d255885ec5299f15556a20aca856cd8d17
PushpakAg <pushpakagrawal123@gmail.com>
2025-03-10 04:29:51 +0530

    fixed port 8080

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def get_post_info():
 
 if __name__ == "__main__":
     # Get port from environment variable or default to 8080
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
